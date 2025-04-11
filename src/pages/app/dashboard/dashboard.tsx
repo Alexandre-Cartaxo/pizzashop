71eb46b0eb71904a05123bc800e1b2b1b91dae39
Alexandre-Cartaxo <alexandre.cartaxo@chacal.dev>
2025-04-11 01:12:41 -0300

    Grafico pedidos da semana

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import { MonthRevenueCard } from './month-revenue-card'
 import { MonthOrdersAmountCard } from './month-orders-amout-card'
 import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard(){
   return (
@@ -16,7 +17,10 @@ export function Dashboard(){
         <MonthOrdersAmountCard/>
         <DayOrdersAmountCard />
         <MonthCanceledOrdersAmountCard/>
-          
+        </div>
+        
+        <div className='grid grid-cols-9 gap-4'>
+            <RevenueChart/>
         </div>
       </div>
     </>
